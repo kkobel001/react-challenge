909821ee01c65195334bdc5065f2fc8f2e1e27bb
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-01-23 20:22:43 +0100

    Add Styled NoContent

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,3 +1,36 @@
+import * as React from 'react';
+import { styled } from '@mui/system';
+import imgNoContent from './../../assets/no_content.png';
+
+const WrapperNoContent= styled('div')({
+  textAlign: 'center',
+});
+const TitleNoContent= styled('h1')({
+  fontSize:'64px',
+  color:'#000000',
+  weight:'700',
+  lineHeight:'76,8px',
+  letterSpacing:'-1',
+});
+
+const StyledP= styled('div')({
+color:'#33333',
+opacity:'50%',
+fontSize: '19,2px',
+lineHeight:'28,8px',});
+
 export const NoContent = () => {
-  return null;
+  return (
+    <WrapperNoContent>
+      <TitleNoContent>NoContent</TitleNoContent>
+      <img
+        src={imgNoContent} 
+        alt="noContent" 
+        style={{
+        width: 202,
+        height: 202,}} 
+      />
+      <StyledP>Brak danych do wyświetlnie</StyledP>
+    </WrapperNoContent>
+  )
 };
