8cd5d07252975c42cd2d7b0d8a86733b93634f36
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-01-31 20:43:52 +0100

    Add last changes to task

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -2,8 +2,14 @@ import * as React from 'react';
 import { Button as MuiButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { styled } from '@mui/system';
 
 
+const StyledIcon= styled(KeyboardArrowRightIcon)({
+  minWidth: '20px',
+  minHeight:'20px',
+  fontSize: 20,
+ });
 
 export function Button({ endIcon, startIcon, children, ...props }) {
 
@@ -11,11 +17,11 @@ export function Button({ endIcon, startIcon, children, ...props }) {
 
     return (
       <MuiButton 
+        style={ {minWidth: emptybutton ? '8px': 'auto'}}
         startIcon={startIcon && <AddIcon />} 
         endIcon={endIcon && <KeyboardArrowRightIcon/> }
         {...props}>
-        { emptybutton ? (<KeyboardArrowRightIcon style={{width:'34px'}}/> ) : (children)  }
-
+        { emptybutton ? (<StyledIcon /> ) : (children)  }
       </MuiButton>
     )
   }
