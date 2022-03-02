fab004fea6390bcc9366de36657195d6ebf70ea3
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-03-02 19:29:59 +0100

    Refactoring task-2

--- client/src/pages/Budget.page.jsx
+++ client/src/pages/Budget.page.jsx
@@ -1,56 +1,13 @@
 import React from 'react';
-import { useQuery } from 'react-query';
-import { ActionHeader, Card, Page,Error,Loader, NoContent , Table, Money, LocalizedDate, CategoryCell} from 'ui';
+import { ActionHeader, Card, Page } from 'ui';
 import { Grid } from '@mui/material';
 import {BudgetTableWidget} from 'ui/organisms/BudgetTable.widget';
 import { Button } from 'ui/atoms/Button';
-import { Box } from '@mui/system';
 
-
+ 
 
  
 export const BudgetPage = () => {
-  const { isLoading, isError, data } = useQuery("repoData", BudgetService.findAll);
-  
-
-  console.log(data, BudgetService.findAll);
-
-  if (isLoading) return "Loading... ";
-
-  if (isError) return "An error has occurred: "
-  
-  // const dataRows= data.map((msg) => (
-  //   <div key={msg.id}>{msg.category.name}</div>
-  // ))
-
-  const headCells =[
-    {
-      id: 1,
-      label:"Nazwa",
-      renderCell: (msg) => <CategoryCell color={msg.category.color} name={msg.category.name}/>},
-    {
-      id: 2,
-      label:"Planowane wydatki",
-      renderCell: (msg) => <Money inCents= {msg.amountInCents} />
-    },
-    {
-      id: 3,
-      label:"Obecna kwota",
-      renderCell: (msg) => <Money inCents= {msg.currentSpending} />
-    },
-    {
-      id: 4,
-      label:"Status",
-      renderCell: (msg) => <Money inCents= {msg.amountInCents} />
-    },
-    {
-      id: 5,
-      label:"Data utworzenia",
-      renderCell: (msg) =>  <LocalizedDate date={msg.createdAt}/>
-    }
-  ]
-
-  const deleteRecords = (n)=> {};
 
   return (
     <Page title="Budżet">
@@ -59,37 +16,18 @@ export const BudgetPage = () => {
           <ActionHeader
             variant={'h1'}
             title="Budżet"
-            renderActions={() =>  <Button startIcon variant={'contained'}>Zdefinuj budet</Button>
+            renderActions={() => <Button startIcon variant={'contained'}>Zdefinuj budet</Button>
             }
           />
         }
       >
         <Grid container>
           <Grid item xs={12}>
-          {/* {isLoading && ( <Loader />) }
-          {isError  && (<Error />)}
-          { !data.length  && (<NoContent />) }
-          <Table
-          headCells={headCells}
-          rows={data} 
-          getUniqueId={(msg) => msg.id} 
-          deleteRecords={deleteRecords} /> */}
-          <BudgetTableWidget />
-        
+             <BudgetTableWidget />
           </Grid>
         </Grid>
       </Card>
     </Page>
   );
 }; 
-
-
-{/* <Table>
-{data.map(msg => (
-    <li key={msg.id}> 
-    {msg.name}
-    {msg.budgetIds}
-    {msg.currentSpending}
-    </li>
-  )}
-</Table> */}
+ 
