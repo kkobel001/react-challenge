4e352b82c8b9fcd11665ade2eb68d3f764662677
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-03-09 23:18:41 +0100

    Add LedgerWidget

--- client/src/ui/organisms/Ledger.widget.jsx
+++ client/src/ui/organisms/Ledger.widget.jsx
@@ -1,17 +1,101 @@
 import React from 'react';
+import {LEDGER_QUERY} from 'queryKeys';
+import { LedgerService } from 'api';
+import { useQuery, useQueryClient, useMutation} from 'react-query';
+import { Money} from 'ui/atoms/Money';
+import { Button} from 'ui/atoms/Button';
+import { LocalizedDate} from 'ui/atoms/LocalizedDate';
+import { Table} from 'ui/molecules/table/Table';
+import { Loader} from 'ui/atoms/Loader';
+import { ActionHeader, Card} from 'ui';
+import { Error } from 'ui/atoms/Error';
+import { NoContent} from 'ui/atoms/NoContent';
+import { CategoryCell} from 'ui/molecules/CategoryCell';
+import Box from '@mui/material/Box';
 
-import { ActionHeader, Card } from 'ui';
+
+
 
 export const LedgerWidget = () => {
-  return (
-    <Card
-      title={
-        <ActionHeader
-          variant={'h1'}
-          title="Portfel"
-          renderActions={() => <></>}
-        />
+
+  const tableDefinition=[
+    {
+      id:'name',
+      label:'Nazwa',
+      renderCell: row => row.title
+    },
+    {
+      id:'categoryId',
+      label:'Kategoria',
+      renderCell: (row) => (
+        <CategoryCell color={row.category?.color} name={row.categorys?.name} /> 
+    )
+    },
+    {
+      id: 'createdAt',
+      label:'Data',
+      renderCell: (row) => (
+        <LocalizedDate date={row.createdAt} />
+        )
+    },
+    {
+      id: 'amount',
+      label:'Kwota',
+      renderCell: (row) => (
+          <Money inCents={row.amountInCents} />
+      )
+  },
+  ]
+    const queryClient= useQueryClient();
+    const { isLoading, error, data } = useQuery(LEDGER_QUERY, () => LedgerService.findAll());
+  
+
+    const mutation = useMutation((ids) => LedgerService.remove({ids}), {
+      onSuccess: async () => {
+          await queryClient.refetchQueries([LEDGER_QUERY]);
       }
-    />
+  })
+  
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (error) {
+    console.log("message", error.message);
+    return <Error error={error} />;
+  }
+  if (!data?.length) {
+    return <NoContent />;
+  }
+  
+  
+  const deleteRecords = (ids) => mutation.mutate(ids);
+  
+  
+  return (
+    
+    <Card
+        title={
+          <ActionHeader
+            variant={'h1'}
+            title="Portfel"
+            renderActions={() => 
+              <Box>
+            <Button startIcon variant={'contained'}>Wpłac</Button>
+            <Button startIcon variant={'contained'}>Wyplac -</Button>
+
+              </Box>
+          }
+            />
+        }  
+    >
+      <Table
+        rows={data} 
+        headCells={tableDefinition}
+        getUniqueId={(row)=> row.id}
+        deleteRecords={deleteRecords}
+        />
+    </Card>
+    
+      
   );
 };
