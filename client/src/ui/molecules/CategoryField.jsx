a974e2844e5cc3caa2465b330b9d4b3c6a10be70
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-05-15 02:01:26 +0200

    Add new funcionals and create CategoryField

---
+++ client/src/ui/molecules/CategoryField.jsx
@@ -0,0 +1,29 @@
+import { MenuItem, TextField } from '@mui/material';
+import React from 'react';
+import { CategoryCell } from 'ui';
+import { PropTypes } from 'prop-types';
+
+
+export const CategoryField = ({value, onChange, categories, ...inputProps}) => {
+    return (
+        <TextField
+        select
+        label="Kategoria"
+        value={value}
+        onchange={onChange}
+        {...inputProps}
+        >
+            {categories.map((option)=>(
+                <MenuItem key={option.id} value={option.id}>
+                    <CategoryCell color={option.color} name={option.name} />
+                </MenuItem>
+            ))}
+        </TextField>
+    )
+}
+
+CategoryField.propTypes={
+    value: PropTypes.string,
+    onChange:PropTypes.func,
+    categories:PropTypes.any,
+};
--- client/src/ui/organisms/AddNewLedgerRecord.modal.jsx
+++ client/src/ui/organisms/AddNewLedgerRecord.modal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { Box, TextField } from '@mui/icons-material';
-import {Modal, CategoryField, Loader, Error, NoContent } from 'ui';
+import { Modal, CategoryField, Loader, Error, NoContent } from 'ui';
 import { formatDollarsToCents } from 'utils';
 import { LedgerService, CategoryService } from 'api';
 import { useQuery, useQueryClient, useMutation } from 'react-query';
-import { PARTIAL_CATEGORIES_QUERY, BUDGET_QUERY, LADGER_QUERY } from 'queryKeys';
+import {
+CATEGORIES_QUERY,
+  BUDGET_QUERY,
+  LEDGER_QUERY,
+} from 'queryKeys';
 import PropTypes from 'prop-types';
 
 const transitionKeys = {
@@ -20,36 +24,128 @@ export const AddNewLedgerRecord = ({ showModal, onClose, type }) => {
     mode: 'onChange',
   });
 
-  const {isLoading, error,data:categories} = useQuery(PARTIAL_CATEGORIES_QUERY, () => CategoryService.findAll())
-
-  const handleClose = () => { 
-    onClose();
-  };
+  const {
+    isLoading,
+    error,
+    data: categories,
+  } = useQuery(CATEGORIES_QUERY, () => CategoryService.findAll());
 
   const mutation = useMutation(
-    (requestBody)=> LedgerService.create({requestBody}),{
+    (requestBody) => LedgerService.create({ requestBody }),
+    {
       onSuccess: async () => {
-        await queryClient.refetchQueries([LADGER_QUERY]);
+        await queryClient.refetchQueries([LEDGER_QUERY]);
         await queryClient.refetchQueries([BUDGET_QUERY]);
         handleClose();
-      }
-    }
-  )
+      },
+    },
+  );
   const onSubmit = async (formData) => {
-    if(!formState.isValid) return;
+    if (!formState.isValid) return;
     mutation.mutate({
-      title:formData.title,
-      amountInCents:formatDollarsToCents(formData.amount),
-      mode:type,
+      title: formData.title,
+      amountInCents: formatDollarsToCents(formData.amount),
+      mode: type,
+    });
+  };
 
-    })
-  }
+
+    const handleClose = () => {
+    onClose();
+  };
   return (
     <Modal
       showModal={showModal}
       onClose={handleClose}
+      canSubmit={handleSubmit(onSubmit)}
       title={`Dodaj ${transitionKeys[type.toLowerCase()]}`}
-    />
+    >
+    { isLoading && <Loader />}
+    { error && <Error error={error} />}
+    { !isLoading && !error && !categories?.length ? (
+      'Wszystkie kategorie są przypisane do budetu.Aby zredefiniowac usuń jeden z wpisów.' 
+
+    ): (
+      <Box component="form" autoComplete='off'>
+        <Controller
+            name="amount"
+            control={control}
+            defaultValue=""
+            rules={{
+              validate:(field) =>{if(!field.toString().trim()){
+                return 'Nazwa nie moze byc pusta'
+              }}
+            }}
+            render={({field: { onChange, value }, fieldState: {error} }) => (
+                <TextField
+                margin={'normal'}
+                type="number"
+                label="Nazwa"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+                />
+             )}
+             />
+             <Controller
+                name="amount"
+                control={control}
+                defaultValue=""
+                
+                rules={{
+                  setValueAs:(value) => value.trim(),
+                      required: {
+                        value: true,
+                        message: 'Kwota nie moze byc pusta',
+                      }, 
+                       min: {
+                        value : 0,
+                        message:'Kwota musi byc większa niz 0'
+                      },
+                      max:{
+                        value:1000000,
+                        message: ' Kwota nie moze byc wieksza niz 1000000'
+                      },
+                }}
+                render={({field: { onChange, value }, fieldState: {error} }) => (
+                  <TextField
+                  margin={'normal'}
+                  type="number"
+                  label="Kwota"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  />
+               )}
+               />
+               {type === 'EXPENSE' && (
+                 <Controller
+                    name='categoryId'
+                    control={control}
+                    defaultValue=""
+                    render={({field:{onChange,value}, fieldState:{error},}) => (
+                        <CategoryField
+                          margin={'normal'}
+                          categories={categories}
+                          value={value}
+                          onChange={onChange}
+                          error={!!error}
+                          helperText={error ? error.message : null}
+                          />
+                    )}
+                    rules={{
+                      required : {
+                        value: true,
+                        message:'Wybierz kategorie',
+                        },
+                      }}
+                />
+                )}
+       </Box>   
+      )}  
+    </Modal>
   );
 };
 
